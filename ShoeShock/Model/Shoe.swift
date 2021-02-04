493a4ad2f4a2c04bad7be020766cf7cb7f963abe
Joe Vargas <joeavargas@outlook.com>
2021-02-04 10:28:01 -0600

    Moved shoe db to Shoe model as as static variable.

--- ShoeShock/Model/Shoe.swift
+++ ShoeShock/Model/Shoe.swift
@@ -24,6 +24,15 @@ struct Shoe {
         self.addedToCart = addedToCart
         self.addedToFavorites = addedToFavorites
     }
+    
+    static var shoeDB: [Shoe] = [
+        Shoe(image: "shoe1.png", name: "Hi-Vis Pro Leather", price: 75.00, description: "Made famous by Julius “Dr. J” Erving, the Pro Leather was part of many game-changing moments. The first to blend high-flying style and performance. The first to take off from the free-throw line. Today, the silhouette has been upgraded for improved fit and function, as well as distinct off-court style. The Pro leather \"Hi-Vis\" features a neon color-pop Star Chevron and outsole, as well as glow-in-the-dark details.", addedToCart: false, addedToFavorites: false),
+        Shoe(image: "shoe2.png", name: "Black Ice Pro Leather L2", price: 85.00, description: "In the '70s, the Pro Leather made a name for itself as one of the most iconic basketball shoes of all time, thanks to its game-making performance features and statement-making style. Today, we're updating the court legend for the streets with a stacked leather build, an exaggerated midsole, and a doubled-up tongue for added style.", addedToCart: false, addedToFavorites: false),
+        Shoe(image: "shoe3.png", name: "Black Ice Pro Leather", price: 75.00, description: "Since its launch on the court in 1976, the Pro Leather has become a basketball icon, made famous by NBA heavy-hitter Dr. J. This edition is made for the streets, with improved fit and function, comfortable SmartFOAM cushioning, and translucent rubber details for a winter-ready look.", addedToCart: false, addedToFavorites: false),
+        Shoe(image: "shoe4.png", name: "OG Pro Leather", price: 75.00, description: "Originally released in '76 and worn by NBA heavy-hitter Dr. J, the Pro Leather has a rich history in the game. We've updated the OG design for improved fit and function, creating an off-court sneaker that stays true to our hoops heritage.", addedToCart: false, addedToFavorites: false),
+        Shoe(image: "shoe5.png", name: "Pro Leather", price: 45.97, description: "Originally released in '76 and worn by NBA heavy-hitter Dr. J, the Pro Leather has a rich history in the game. Retooled for everyday wear, this edition comes with comfy SmartFOAM cushioning and a tonal colorblocked design.", addedToCart: false, addedToFavorites: false),
+        Shoe(image: "shoe6.png", name: "Heart of the City Chuck 70", price: 85.00, description: "A city emits electric energy. It’s diverse. A melting pot of culture, art, ideas, food, nightlife, architecture. But what makes it pulse? The noise? The lights? The skyline? That buzzer beater type hustle? Nah..it’s us. We’re the heart of this city. Inspired by the courts, street culture and nightlife of Shanghai.", addedToCart: false, addedToFavorites: false)
+    ]
 }
 
 class ShoesInCart: Equatable {
